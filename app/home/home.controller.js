b34f0340aa4d56999f039258a13e86f59749db11
Twana Daniel <twanaazi@ie.ibm.com>
2017-07-19 18:33:47 +0100

    Merging code for #4 and commit code for #5

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,57 +1,141 @@
 (function () {
 
-  'use strict';
+    'use strict';
 
-  angular
-    .module('app')
-    .controller('HomeController', homeController);
+    angular
+        .module('app')
+        .controller('HomeController', homeController);
 
-  function homeController($scope, authService, $location) {
-    var vm = this;
-    vm.auth = authService;
-    vm.login = login;
 
-    // login function making a call to signin that comes from auth service and send the user profile to the profile that user is logged in with. If all good the storing user profile on local storage.
-    function login() {
-        //profile will hold user profile info and the token will get us the jwt
-        auth.signin({}, function (profile, token) {
-            store.set('profile', profile);
-            store.set('id_token', token);
-            $location.path('/test'); // user is sent to home page
-        }, function (error){
-            console.log(error);
-        });
-    }
-
-     // porfolio function
-    function createPortfolio()
-    {
-      $http({
-          method: 'POST',
-          url: '/api/dashboard', 
-          
-            })
-            .then(function(response) {
-                //Success handling
-            }, function(err) {
-                //Error handling
+    function homeController($scope, authService, $http, $location) {
+        var vm = this;
+        vm.auth = authService;
+        vm.login = login;
+        $scope.oneAtATime = true;
+        var mapping = [
+            { name: 'technology',file: '/data/technology_holdings.json'}];
+        var holdingsArray = [];
+        // login function making a call to signin that comes from auth service and send the user profile to the profile that user is logged in with. If all good the storing user profile on local storage.
+        function login() {
+            //profile will hold user profile info and the token will get us the jwt
+            auth.signin({}, function (profile, token) {
+                store.set('profile', profile);
+                store.set('id_token', token);
+                $location.path('/test'); // user is sent to home page
+            }, function (error){
+                console.log(error);
             });
-    }
-    function listPortfolios()
-    {
-      $http({
-             method: 'GET',
-             url: '/api/dashboard', 
-            })
-            .then(function(response) {
-                console.log("RESULT:"+ response);
-            }, function(err) {
-                //Error handling
+        }
+
+// porfolio function
+        vm.initialSetup = function ()
+        {
+            //Reading Porfolios JSON
+            $http({
+                method: 'GET',
+                url: '/data/portfolios.json'
+            }).then(function(result){
+                $scope.portfolios = result.data.portfolios;
+
+                if(result !== null && (result.data !== null || result.data !== ''))
+                {
+                    //console.log(replaceAll(JSON.stringify(result.data),"currentdate",currentISOTimestamp()));
+                    //Creating Porfolios
+                    $http({
+                        method: 'POST',
+                        url: '/api/bulkportfolios',
+                        data: replaceAll(JSON.stringify(result.data),"currentdate",currentISOTimestamp())
+                    })
+                        .then(function(response) {
+                            //Reading Holdings JSON one at a time.
+                            angular.forEach(mapping,function(value,key){
+                                    $http({
+                                        method: 'GET',
+                                        url: value.file
+                                    })
+                                        .then(function(holdings){
+                                            console.log("HOLDINGS JSON:" + holdings.data);
+                                            //Creating Holdings and mapping them to respective portfolio
+                                            $http({
+                                                method: 'POST',
+                                                url: '/api/holdings/'+ value.name,
+                                                data: holdings.data
+                                            })
+                                                .then(function(holdingsresult){
+                                                    console.log("HOLDINGS CREATION result:" + holdingsresult.data);
+                                                },function(err) {
+                                                    console.log("CREATE HOLDINGS FAILED");
+                                                });
+                                            holdingsArray.push(holdings.data.holdings);
+                                        },function(err) {
+                                        });
+
+                                }
+                            );
+                            $scope.holdings = holdingsArray;
+                            console.log("HOLDINGS ARRAY:" + holdingsArray);
+                        }, function(err) {
+                            console.log("CREATE PORTFOLIOS FAILED");
+                        });
+                }
+
+                //return replaceAll(JSON.stringify(result.data),"currentdate",currentISOTimestamp());
+            },function(err){
+
             });
+
+        }
+        function listPortfolios()
+        {
+            $http({
+                method: 'GET',
+                url: '/api/portfolios',
+            })
+                .then(function(response) {
+                    console.log("RESULT:"+ response);
+                }, function(err) {
+                    //Error handling
+                });
+        }
+
+        vm.getHoldings = function (portfolio)
+        {
+            $scope.holdings = "";
+            switch(portfolio.name){
+                case 'technology':
+                    //alert("technology");
+                    $http({
+                        method: 'GET',
+                        url: '/data/technology_holdings.json'
+                    })
+                        .then(function(holdings){
+                            $scope.holdings = holdings.data.holdings;
+                        });
+                    break;
+
+                case 'pharmaceutical':
+                {
+                    //alert("pharma");
+                    $http({
+                        method: 'GET',
+                        url: '/data/pharma_holdings.json'
+                    })
+                        .then(function(holdings){
+                            $scope.holdings = holdings.data.holdings;
+                        });
+                    break;
+                }
+            }
+        }
+
     }
 
-
-  }
-  homeController.$inject = ['$scope', 'authService'];
+    function currentISOTimestamp(){
+        return new Date().toISOString();
+    }
+    function replaceAll(str, find, replace) {
+        return str.replace(new RegExp(find, 'g'), replace);
+    }
+    homeController.$inject = ['$scope', 'authService', '$http'];
 
 })();
--- server.js
+++ server.js
@@ -4,16 +4,25 @@ const path = require('path');
 const cfenv = require('cfenv');
 const http = require('https');
 const bodyParser = require('body-parser');
+const DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
 
-//--Config------------------------------
+var port = process.env.VCAP_APP_PORT || 3000;
+var vcapLocal = null;
+
+//--Temp setup of discovery service--------------------
+// var discovery = new DiscoveryV1({
+//     username: '',
+//     password: '',
+//     version_date: '2017-07-19'
+// });
+
+//--Config--------------------
 require('dotenv').config();
 
-//---Deployment Tracker---------------------------------------------------------
+//--Deployment Tracker--------------------
 require("cf-deployment-tracker-client").track();
 
-// configuration ===============================================================
-// load local VCAP configuration
-var vcapLocal = null
+//--load local VCAP configuration--------------------
 if (require('fs').existsSync('./vcap-local.json')) {
     try {
         vcapLocal = require("./vcap-local.json");
@@ -23,7 +32,7 @@ if (require('fs').existsSync('./vcap-local.json')) {
     }
 }
 
-// get the app environment from Cloud Foundry, defaulting to local VCAP
+//--Get the app environment from Cloud Foundry, defaulting to local VCAP--------------------
 var appEnvOpts = vcapLocal ? {
     vcap: vcapLocal
 } : {}
@@ -32,18 +41,15 @@ var appEnv = cfenv.getAppEnv(appEnvOpts);
 if (appEnv.isLocal) {
     require('dotenv').load();
 }
-var port = process.env.VCAP_APP_PORT || 3000;
 
-// Main routes
+//--Setting up the middle ware--------------------
 app.use('/', express.static(__dirname +  '/'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-// =====================================
-// INVESTMENT PORTFOLIO SECTION =====================
-// =====================================
-//Portfolios POST & GET Methods
+
+//--Portfolios POST Methods - To Create single portfolios--------------------
 app.post('/api/portfolios', function(req, response){
     console.log("REQUEST:" + req.body.porfolioname);
     var basic_auth= toBase64();
@@ -82,6 +88,43 @@ app.post('/api/portfolios', function(req, response){
     req.end();
 });
 
+//Portfolios POST Methods - To Create multiple portfolios
+app.post('/api/bulkportfolios', function(req, response){
+    var basic_auth= toBase64();
+    var requestBody = req.body;
+    console.log("REQUESTBODY" + JSON.stringify(requestBody));
+    var options = {
+        "method": "POST",
+        "hostname": process.env.INVESTMENT_PORFOLIO_BASE_URL,//"investment-portfolio.mybluemix.net",
+        "port": null,
+        "path": "/api/v1/bulk_portfolios",
+        "headers": {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "authorization": "Basic "+basic_auth
+        }
+    };
+
+    var req = http.request(options, function (res) {
+        var chunks = [];
+        console.log("AUTH:" + basic_auth);
+
+        res.on("data", function (chunk) {
+            chunks.push(chunk);
+        });
+
+        res.on("end", function () {
+            var body = Buffer.concat(chunks);
+            console.log(body.toString());
+            response.end(body.toString());
+        });
+    });
+
+    req.write(JSON.stringify(requestBody));
+    req.end();
+});
+
+//--Portfolios GET Methods--------------------
 app.get('/api/portfolios',function(req,response){
     const basic_auth= toBase64();
     var islatest = req.query.latest || true;
@@ -119,7 +162,7 @@ app.get('/api/portfolios',function(req,response){
     req.end();
 });
 
-//Holdings POST & GET methods
+//--Holdings POST methods--------------------
 app.post("/api/holdings/:porfolioname",function (request,response){
     const basic_auth = toBase64();
     var portfolioname = request.params.porfolioname || "default";
@@ -151,10 +194,11 @@ app.post("/api/holdings/:porfolioname",function (request,response){
     });
 
     req.write(JSON.stringify({ holdings: holdings, timestamp: currentISOTimestamp() }));
-//console.log(JSON.stringify({ holdings: holdings, timestamp: currentISOTimestamp() }));
+    //console.log(JSON.stringify({ holdings: holdings, timestamp: currentISOTimestamp() }));
     req.end();
 });
 
+//--Holdings GET methods--------------------
 app.get("/api/holdings/:portfolioname",function(request,response){
     const basic_auth = toBase64();
     var portfolioname = request.params.portfolioname || "default";
@@ -190,14 +234,31 @@ app.get("/api/holdings/:portfolioname",function(request,response){
     req.end();
 });
 
+
+//--Discovery News GET--------------------
+// app.get('/api/news',function(req,res){
+//     discovery.query({
+//         environment_id: '7194d449-dc7e-4f82-939c-3c9205a0a701',
+//         collection_id: '4f444003-d770-4c27-8e17-8f6b43d9952a',
+//         query: 'entities.text:IBM'
+//     }, function(err, response) {
+//         if (err) {
+//             console.error(err);
+//         } else {
+//             console.log(JSON.stringify(response, null, 2));
+//             res.json(response);
+//         }
+//     });
+// });
+
+
+//--All other routes to be sent to home page--------------------
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-//---------------------------
-// PRIVATE FUNCTIONS
-//---------------------------
-//To generate basic authorization
+
+//--BASE FUNCTION FOR authorization used by the INVESTMENT PORTFOLIO service--------------------
 function toBase64()
 {
     var basic_auth= new Buffer(process.env.INVESTMENT_PORFOLIO_USERNAME + ':' + process.env.INVESTMENT_PORFOLIO_PASSWORD).toString('base64');
@@ -208,7 +269,9 @@ function currentISOTimestamp()
 {
     return new Date().toISOString();
 }
-// launch ======================================================================
+
+
+//--launch--------------------
 app.listen(port, "0.0.0.0", function() {
     // print a message when the server starts listening
     console.log("server running on  http://localhost:" + port);
