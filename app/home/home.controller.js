9cf2bf308e26ac774501a64c3c259d75ffa548e6
VidyasagarMSC <mscvidyasagar@gmail.com>
2017-07-21 10:31:56 +0530

    Merging with master branch

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -143,10 +143,10 @@
 
     }
 
-  vm.toDiscovery = function(companyname)
+  /*vm.toDiscovery = function(companyname)
   {
       //alert(companyname);
-  }
+  }*/
 
 
     homeController.$inject = ['$scope', 'authService', '$http'];
