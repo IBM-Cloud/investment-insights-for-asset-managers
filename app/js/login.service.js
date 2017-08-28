8cd7acc88c05f544ae47c016fddd88380f5abe2d
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-08-28 09:29:17 +0200

    cache response if logged=true

--- app/js/login.service.js
+++ app/js/login.service.js
@@ -4,10 +4,20 @@
 
   function LoginService($http, $q) {
     console.log('LoginService loading...');
+    var lastResponse;
     return {
       isLogged: function() {
+        if (lastResponse && lastResponse.logged) {
+          console.debug('Reusing last response');
+          return $q.resolve(lastResponse);
+        }
+
         var deferred = $q.defer();
         $http.get('/auth/logged').then(function(response) {
+          // stop calling the API once we know we are logged
+          if (response.data.logged) {
+            lastResponse = response.data;
+          }
           deferred.resolve(response.data);
         }).catch(function(err) {
           console.log(err);
